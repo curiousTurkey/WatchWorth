d9677a1e5252782a4f40313ce0b06af4add854c4
Eldhose Babu <eldhosebabu197@gmail.com>
2024-11-03 18:33:23 -0500

    Initial Commit

---
+++ WatchWorth/WatchWorthApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchWorthApp.swift
+//  WatchWorth
+//
+//  Created by Eldhose on 2024-11-03.
+//
+
+import SwiftUI
+
+@main
+struct WatchWorthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
